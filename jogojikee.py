df438eac7697d502334d19eac01c05b7e9dc8708
ifeslopes <ifes.lopes@gmail.com>
2019-12-27 20:55:52 -0300

    Jogo JO-KEN-POO

---
+++ jogojikee.py
@@ -0,0 +1,58 @@
+from  random import randint
+from  time import sleep
+from  os import system
+
+def jokepo(x):
+    
+    lista = ["PEDRA","PAPEL","TESOLRA"]
+    comp = randint(0,2)
+ 
+    jogador= int(x)
+    if jogador >2:
+        print("valor invalido")
+        sleep(1)
+        system("clear") or None 
+        
+    else:
+        print("JO")
+        sleep(1)
+        print("KEEEE...")
+        sleep(2)
+        print("POOOO!")
+        sleep(1)
+        
+        print(f"jogador escolheu >>> {lista[jogador]}")
+        print(f"COMPUTADIR escolheu >>>{lista[comp]}")
+        if comp ==0:
+            if jogador ==0:
+                print("EMPATE")
+            elif jogador ==1:
+                print("JOGADOR VENCE")
+            elif jogador ==2:
+                print("COMPUTADOR VENCE")
+        
+        elif comp == 1:
+            if jogador ==0:
+                print("COMPUTADOR VENCE")
+            elif jogador ==1:
+                print("EMPATE")
+            elif jogador ==2:
+                print("JOGADOR VENCE")
+        
+        elif comp ==2:
+            if jogador ==0:
+                print("JOGADOR VENCE")
+            elif jogador ==1:
+                print("COMPUTADOR VENCE")
+            elif jogador ==2:
+                print("EMPATE")
+    
+    
+while True:
+    print()
+    print('''ESCOLHA UMA DAS OPEÇES
+    [0] PEDRA
+    [1] PAPEL
+    [2] TESOLRA''')
+    jogador= int(input("Escolha...: "))
+    jokepo(jogador)
